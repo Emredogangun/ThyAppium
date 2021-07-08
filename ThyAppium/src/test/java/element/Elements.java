b5f8abf1d8a903b25e5a722821acef7130c69cdf
EMRE DOĞANGÜN <50294340+Emredogangun@users.noreply.github.com>
2021-07-08 10:28:28 +0300

    updated

--- ThyAppium/src/test/java/element/Elements.java
+++
@@ -1,9 +0,0 @@
-package element;
-
-import io.appium.java_client.AppiumDriver;
-import io.appium.java_client.MobileElement;
-
-public class Elements {
-
-    protected static AppiumDriver<MobileElement> driver;
-}
